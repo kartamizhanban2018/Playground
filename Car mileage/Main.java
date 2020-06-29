73fe39925860b690cc3291e444318b333ec6ed73
kartamizhanban2018 <60053827+kartamizhanban2018@users.noreply.github.com>
2020-06-29 12:44:09 +0530

    Test code pushed to repository.

---
+++ Car mileage/Main.java
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+ float m;
+
+ int p,d;
+
+ cin>>m>>p>>d;
+
+ if((m*p) >= d)
+
+   cout<<"Can reach";
+
+ else
+
+   cout<<"Cannot reach";
+
+}
